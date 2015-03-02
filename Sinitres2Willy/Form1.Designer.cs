8b4834b3255af606152c5a52c8265230151f533d
Patrice Maldi <patrice.maldi@keolis.com>
2015-03-02 15:55:40 +0100

    Add

---
+++ Sinitres2Willy/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Sinitres2Willy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(670, 361);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(393, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 438);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Sinitres2Willy/Form1.cs
@@ -0,0 +1,115 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinitres2Willy
+{
+    public partial class Form1 : Form
+    {
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connstring = String.Format("Server={0};Port={1};" +
+                    "User Id={2};Password={3};Database={4};",
+                    "mdmkpa", "5432", "postgres",
+                    "postgres", "vehicules_sinistre");
+                NpgsqlConnection conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                string date = DateTime.Now.AddDays(-1).ToString("dd/MM/yyyy");
+                int all = 0;
+                string sql;
+                if (all == 1){
+                    sql = "SELECT * FROM vehicules";
+                }
+                else { 
+                    sql = "SELECT * FROM vehicules WHERE date = '" + date + "'";
+                }
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+                ds.Reset();
+                da.Fill(ds);
+                dt = ds.Tables[0];
+                dataGridView1.DataSource = dt;
+                conn.Close();
+                button2.PerformClick();
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.ToString());
+                throw;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            SmtpClient Smtp_Server = new SmtpClient();
+            MailMessage e_mail = new MailMessage();
+            Smtp_Server.EnableSsl = true;
+            Smtp_Server.UseDefaultCredentials = true;
+            Smtp_Server.Credentials = new System.Net.NetworkCredential("pmaldi", "Ilete1fois");
+            Smtp_Server.DeliveryMethod = SmtpDeliveryMethod.Network;
+            Smtp_Server.Port = 25;
+            Smtp_Server.Host = "webmail.keolis.com";
+
+            e_mail = new MailMessage();
+            e_mail.From = new MailAddress("patrice.maldi@keolis.com");
+            e_mail.To.Add("patrice.maldi@keolis.com");
+            e_mail.To.Add("sylvain.mennillo@keolis.com");
+            e_mail.To.Add("willy.boisfer@keolis.com");
+            //e_mail.To.Add("philippe.boulet@keolis.com");
+            e_mail.Subject = "Sinistralité : " + System.DateTime.Now + "";
+            e_mail.IsBodyHtml = true;
+            if (dataGridView1.Rows.Count != 0)
+            {
+                sb.AppendLine("<center><img src='K:\\Commun\\Keolis_logo.png'><br><br>Date du rapport : <b>" + System.DateTime.Now + "</b>");
+                sb.AppendLine("<center><b>Nouveau Sinistre : </b></center><br>");
+                sb.AppendLine("<style type='text/css'>table.border_1_solid_black td { border : 1px solid black; }</style>");
+                sb.AppendLine("<br><br><table class='border_1_solid_black'><tr><td align='center' valign='middle'>Numero du Sinitre :</td><td align='center' valign='middle'>Numero de Bus :</td><td align='center' valign='middle'>Motif :</td><td align='center' valign='middle'>Controleur :</td><td align='center' valign='middle'>Date :</td><td align='center' valign='middle'>Heure :</td></tr>");
+                foreach (DataGridViewRow rowView in dataGridView1.Rows)
+                {
+                    string num = rowView.Cells["id"].Value.ToString();
+                    string bus = rowView.Cells["vehicule"].Value.ToString();
+                    string motif = rowView.Cells["motif"].Value.ToString();
+                    string controleur = rowView.Cells["controleur"].Value.ToString();
+                    string date = rowView.Cells["date"].Value.ToString();
+                    string Heure = rowView.Cells["Heure"].Value.ToString();
+                    byte[] data = Convert.FromBase64String(motif);
+                    string decodedString = Encoding.UTF8.GetString(data);
+                    string decodedStringHTML = decodedString.Replace("%20", " ");
+                    sb.AppendLine("<tr><td align='center' valign='middle'>" + num + "</td><td align='center' valign='middle'>" + bus + "</td><td align='center' valign='middle'>" + decodedStringHTML + "</td><td align='center' valign='middle'>" + controleur + "</td><td align='center' valign='middle'>" + date + "</td><td align='center' valign='middle'>" + Heure + "</td></tr>");
+                }
+                sb.AppendLine("</table><br><br><i>Note : Attention le nom du contrôleur n'est pas encore validé, par conséquent le contrôleur par défaut est Willy BOISFER.");
+            }
+            else
+            {
+                sb.AppendLine("<center><img src='K:\\Commun\\Keolis_logo.png'><br><br>Date du rapport : <b>" + System.DateTime.Now + "</b>");
+                sb.AppendLine("<br><br><h1><center><b>Il n'y a pas de nouveau Sinistre !</b></center><br><br>");
+                sb.AppendLine("<center><img src='K:\\Commun\\pouce.png'>");
+            }                
+            e_mail.Body = sb.ToString();
+            Smtp_Server.Send(e_mail);
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.PerformClick();
+        }
+    }
+}
