1351649c52dbe141726c340218e27e43f98d44e4
Patrice Maldi <patrice.maldi@keolis.com>
2015-03-02 16:41:14 +0100

    ajout lien email

--- Sinitres2Willy/Form1.cs
+++ Sinitres2Willy/Form1.cs
@@ -31,14 +31,13 @@ namespace Sinitres2Willy
                     "postgres", "vehicules_sinistre");
                 NpgsqlConnection conn = new NpgsqlConnection(connstring);
                 conn.Open();
-                string date = DateTime.Now.AddDays(-1).ToString("dd/MM/yyyy");
                 int all = 0;
                 string sql;
                 if (all == 1){
                     sql = "SELECT * FROM vehicules";
                 }
                 else { 
-                    sql = "SELECT * FROM vehicules WHERE date = '" + date + "'";
+                    sql = "SELECT * FROM vehicules WHERE constat = '0' AND archive = '0'";
                 }
                 NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
                 ds.Reset();
@@ -72,13 +71,14 @@ namespace Sinitres2Willy
             e_mail.To.Add("patrice.maldi@keolis.com");
             e_mail.To.Add("sylvain.mennillo@keolis.com");
             e_mail.To.Add("willy.boisfer@keolis.com");
+            e_mail.To.Add("alison.cavelier@keolis.com");
             //e_mail.To.Add("philippe.boulet@keolis.com");
             e_mail.Subject = "Sinistralité : " + System.DateTime.Now + "";
             e_mail.IsBodyHtml = true;
             if (dataGridView1.Rows.Count != 0)
             {
                 sb.AppendLine("<center><img src='K:\\Commun\\Keolis_logo.png'><br><br>Date du rapport : <b>" + System.DateTime.Now + "</b>");
-                sb.AppendLine("<center><b>Nouveau Sinistre : </b></center><br>");
+                sb.AppendLine("<center><b>Liste des sinistres sans Constat & Numéro Keorisk : </b></center><br>");
                 sb.AppendLine("<style type='text/css'>table.border_1_solid_black td { border : 1px solid black; }</style>");
                 sb.AppendLine("<br><br><table class='border_1_solid_black'><tr><td align='center' valign='middle'>Numero du Sinitre :</td><td align='center' valign='middle'>Numero de Bus :</td><td align='center' valign='middle'>Motif :</td><td align='center' valign='middle'>Controleur :</td><td align='center' valign='middle'>Date :</td><td align='center' valign='middle'>Heure :</td></tr>");
                 foreach (DataGridViewRow rowView in dataGridView1.Rows)
@@ -90,9 +90,19 @@ namespace Sinitres2Willy
                     string date = rowView.Cells["date"].Value.ToString();
                     string Heure = rowView.Cells["Heure"].Value.ToString();
                     byte[] data = Convert.FromBase64String(motif);
+
+                    string connstring = String.Format("Server={0};Port={1};" +
+                    "User Id={2};Password={3};Database={4};",
+                    "mdmkpa", "5432", "postgres",
+                    "postgres", "vehicules");
+                    NpgsqlConnection conn = new NpgsqlConnection(connstring);
+                    conn.Open();
+                    string sql = "SELECT modele FROM vehicules.vehicule WHERE parc_keolis = '" + bus + "'";
+                    NpgsqlCommand command = new NpgsqlCommand(sql, conn);
+                    string modele = command.ExecuteScalar().ToString();
                     string decodedString = Encoding.UTF8.GetString(data);
-                    string decodedStringHTML = decodedString.Replace("%20", " ");
-                    sb.AppendLine("<tr><td align='center' valign='middle'>" + num + "</td><td align='center' valign='middle'>" + bus + "</td><td align='center' valign='middle'>" + decodedStringHTML + "</td><td align='center' valign='middle'>" + controleur + "</td><td align='center' valign='middle'>" + date + "</td><td align='center' valign='middle'>" + Heure + "</td></tr>");
+                    string decodedStringHTML = decodedString.Replace("%20", " ").Replace("%E9", "é").Replace("%2C", ",").Replace("%29", ")").Replace("%28", "(");
+                    sb.AppendLine("<tr><td align='center' valign='middle'>" + num + "</td><td align='center' valign='middle'><a href='http://mdmkpa/sinistre/index.php?bus="+bus+"&modele="+modele+"&controleur=Willy%20Boisfer'>" + bus + "</a></td><td align='center' valign='middle'>" + decodedStringHTML + "</td><td align='center' valign='middle'>" + controleur + "</td><td align='center' valign='middle'>" + date + "</td><td align='center' valign='middle'>" + Heure + "</td></tr>");
                 }
                 sb.AppendLine("</table><br><br><i>Note : Attention le nom du contrôleur n'est pas encore validé, par conséquent le contrôleur par défaut est Willy BOISFER.");
             }
